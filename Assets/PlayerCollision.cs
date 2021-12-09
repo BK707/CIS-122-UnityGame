f3f012dabc2db5c054bdda8af3e2d796594d81b6
samandgreeneggs <94149439+samandgreeneggs@users.noreply.github.com>
2021-12-09 17:59:42 -0600

    FinishLine 2

--- Assets/PlayerCollision.cs
+++ Assets/PlayerCollision.cs
@@ -8,16 +8,32 @@ using UnityEngine.UI;
 public class PlayerCollision : MonoBehaviour
 {
 
-    //public PlayerMovement movement;
+    public PlayerMovement movement;
     //public RunningScore runningScore;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
+        //if player collides with an obstacle, take points away
         if (collisionInfo.collider.tag == "Obstacle")
         {
-            //movement.enabled = false;
+            
             RunningScore.instance.RemovePoints();
         }
+        //if player collides with finish line...
+        if(collisionInfo.collider.tag == "FinishLine")
+        {
+            movement.enabled = false;
+            /*
+            if(score >= 50)
+            {
+
+            }
+            else
+            {
+
+            */
+        }
+
     }
 
 
